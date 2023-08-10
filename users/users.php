b7e9db685393a947f829e21d03738b849007ace3
Sushmareddy589 <137196309+Sushmareddy589@users.noreply.github.com>
2023-08-10 16:24:53 +0530

    Update users.php

--- users/users.php
+++ users/users.php
@@ -1,16 +1,26 @@
+
 <?php
 require "../config.php";
-$stmt = $conn->prepare("SELECT employee_id,emp_name,emp_email,emp_username,age,gender FROM employees");
-$stmt->execute();
-$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
+try {
+    $query = "SELECT employee_id,emp_name,emp_email,emp_username FROM employees";
+    $prepared = $conn->prepare($query);
+    $prepared->execute();
+    $result = $prepared->fetchAll(PDO::FETCH_ASSOC);
+?>
+    <!DOCTYPE html>
+    <html>
+    <table border="1" cellpadding="10" cellspacing="0">
 
-?> 
-<!DOCTYPE html>
-<html>
+ 
 
-<body>
+        <head>
+            <title>Employees_list</title>
 
+        </head>
+
+        <body>
+            <div>
             <div align="center">
                 <a href="http://localhost/sample/users/add_users.php">
                     Register
@@ -18,63 +28,68 @@ $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
                 <br>
                 <br>
             </div>
-    <table border="1px">
-        <thead>
-            <td>
-                Emp ID
-            </td>
-            <td>
-                Emp Name
-            </td>
-            
-            <td>
-                Emp Username
-            </td>
-            
-            <td>
-                action
-            </td>
+                <div>
+                    <table border="1px">
+                        <thead>
+                            <td>
+                                Emp ID
+                            </td>
+                            <td>
+                                Emp Name
+                            </td>
+                            <td>
+                                Emp Email
+                            </td>
+                            <td>
+                                Emp Username
+                            </td>
+                            <td>
+                                action
+                            </td>
 
 
 
-        </thead>
+                        </thead>  
+                        <tbody>
 
-        <tbody>
+                            <tr>
+                                <?php
+                                $sn = 1;
+                                foreach ($result as $data) {
 
+                                ?>
+                            <tr>
 
-            <?php
-        // checking wether data found
-            if ($stmt->fetch()) {
+                                <td><?php echo $data['employee_id']; ?> </td>
+                                <td><?php echo $data['emp_name']; ?> </td>
+                                <td><?php echo $data['emp_email']; ?> </td>
+                                <td><?php echo $data['emp_username']; ?> </td>
+                                <td><a href="view.php?employee_id=<?php echo $data['employee_id']; ?>">view </a>
+                                    <a href="delete.php?employee_id=<?php echo $data['employee_id']; ?>">delete </a>
+                                    <a href="update.php?employee_id=<?php echo $data['employee_id']; ?>">update</a>
+                                </td>
+                            </tr>
 
-              // printing user data
-                while ($data = $stmt->fetch()) {
-
-            ?>
-                    <tr>
-
-                        <td><?php echo $data['employee_id']; ?> </td>
-                        <td><?php echo $data['emp_name']; ?> </td>
-                        <td><?php echo $data['emp_username']; ?> </td>
-                       
-                        <td>
-                            <a href="update_user.php?employee_id=<?php echo $data['employee_id']; ?>">update</a>
-                            <a href="delete_user.php?employee_id=<?php echo $data['employee_id']; ?>">delete </a>
-
-                        </td>
-                    </tr>
-        </tbody>
-<?php
-                }
-            } 
-            else {
-                echo " User not Found !!";
+                        <?php
+                                }
+                        ?>
+                    </table>
+                <?php
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
             }
-?>
+                ?>
+                </tr>
+
+
+
+
+                </tbody>
     </table>
+    </div>
 
-</body>
+    </div>
 
+    </body>
 
-
-
-</html>
+    </html>
